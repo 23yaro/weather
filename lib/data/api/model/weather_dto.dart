0193aa594fe14deff9fbac66075b18f5fa2a007d
Alexander <sanchesasduio@gmail.com>
2024-12-25 02:51:54 +0300

    weather dto

---
+++ lib/data/api/model/weather_dto.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'weather_dto.freezed.dart';
+part 'weather_dto.g.dart';
+
+@freezed
+class WeatherDTO with _$WeatherDTO {
+  const factory WeatherDTO({
+    required int temp,
+    required String condition,
+    @JsonKey(name: "feels_like") required int feelsLike,
+    @JsonKey(name: "wind_speed") required int windSpeed,
+    required int humidity,
+  }) = _WeatherDTO;
+
+  factory WeatherDTO.fromJson(Map<String, Object?> json)
+  => _$WeatherDTOFromJson(json);
+}
