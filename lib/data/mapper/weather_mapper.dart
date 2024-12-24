e940b4517903ffb990a9049cf56dff2ce2a5ba7a
Alexander <sanchesasduio@gmail.com>
2024-12-25 02:52:30 +0300

    weather mapper dto to domen model

---
+++ lib/data/mapper/weather_mapper.dart
@@ -0,0 +1,14 @@
+import 'package:weather/data/api/model/weather_dto.dart';
+import 'package:weather/domain/model/weather.dart';
+
+class WeatherMapper {
+  static Weather fromDTO(WeatherDTO weatherDTO) {
+    return Weather(
+      temp: weatherDTO.temp,
+      condition: weatherDTO.condition,
+      feelsLike: weatherDTO.feelsLike,
+      windSpeed: weatherDTO.windSpeed,
+      humidity: weatherDTO.humidity,
+    );
+  }
+}
