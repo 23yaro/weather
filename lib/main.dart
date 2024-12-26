ff43cb1956d251ff5b52aa91ca21a6c33b281d72
Alexander <sanchesasduio@gmail.com>
2024-12-27 00:36:15 +0300

    di to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
-import 'package:weather/data/api/service/location_service/location_service.dart';
-import 'package:weather/data/api/service/permission_service/permission_service.dart';
-import 'package:weather/data/api/service/weather_service/weather_service.dart';
-import 'package:weather/data/repository/weather_repository.dart';
+import 'package:weather/di/di_container.dart';
+import 'package:weather/domain/domain.dart';
 import 'package:weather/domain/usecase/weather_usecase.dart';
-import 'package:weather/domain/view_model/home_vm.dart';
 import 'package:weather/presentation/home_view.dart';
 
 void main() {
+  setupGetIt();
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late final WeatherUseCase weatherUseCase;
-
-  @override
-  void initState() {
-    weatherUseCase = WeatherUseCase(
-      weatherRepository: WeatherRepository(
-        locationService: LocationService(geolocator: Geolocator()),
-        permissionService: PermissionService(geolocator: Geolocator()),
-        weatherService: WeatherService(),
-      ),
-    );
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final weatherUseCase = GetIt.instance<WeatherUseCase>();
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
